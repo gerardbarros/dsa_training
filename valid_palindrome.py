3d2784f11df957c6d687374bd59f60443dd9be13
Gerard Barros <barros.gc@gmail.com>
2024-05-02 21:29:33 -0500

    Create function that determines if word or phrase is a palindrome using two pointers or built in functions

---
+++ valid_palindrome.py
@@ -0,0 +1,58 @@
+'''
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers. Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "left, right
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+Constraints:
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+'''
+
+def validPalindrome(str):
+    newStr = ''
+
+    for char in str:
+        if char.isalnum():
+            newStr += char.lower()
+    return newStr == newStr[::-1]
+
+# Solution using pointers:
+# def validPalindrome(str):
+#     l,r = 0, len(str) - 1
+
+#     while l < r:
+#         while l and not alphaNum(str[l]):
+#             l += 1
+#         while r > l and not alphaNum(str[r]):
+#             r -= 1
+#         if str[l].lower() != str[r].lower():
+#             return False
+#         l, r = l + 1, r - 1
+#     return True
+
+# def alphaNum(char):
+#     (ord('A') <= ord(char) <= ord('Z') or 
+#         ord('a') <= ord(char) <= ord('z') or 
+#         ord('0') <= ord(char) <= ord('9'))
+
+
+print(validPalindrome('A man, a plan, a canal: Panama'))
+print(validPalindrome('Racecar'))
+print(validPalindrome('Taco cat'))
+print(validPalindrome('Sphinx'))
