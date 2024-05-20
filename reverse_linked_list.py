4b793e31faeddb8e12281bf3f46094fc54fc88de
Gerard Barros <barros.gc@gmail.com>
2024-05-19 23:42:01 -0500

    Create function that iterates through a linked list and reverses it using recursion

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -15,20 +15,31 @@ Output: []
 '''
 
 # ITERATIVE SOLUTION
-def reverseList(head):
-    prev = None
-    curr = head
+# def reverseList(head):
+#     prev = None
+#     curr = head
 
-    while curr:
-        #temp var
-        nxt = curr.next
-        curr.next = prev
-        prev = curr
-        curr = nxt
-    return prev
+#     while curr:
+#         #temp var
+#         nxt = curr.next
+#         curr.next = prev
+#         prev = curr
+#         curr = nxt
+#     return prev
 
 # RECURSIVE SOLUTION - needs extra memory
-# def reverseList(head):
+def reverseList(head):
+
+    if not head:
+        return None
+    
+    newHead = head
+    if head.next:
+        newHead = self.reverseList(head.next)
+        # reverse link from next node and head
+        head.next.next = head
+    head.next = None
+    return newHead
 
 print(reverseList([1, 2, 3, 4, 5]))
 print(reverseList([1, 2]))
